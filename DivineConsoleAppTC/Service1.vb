2265557b6132d7cbd4728cf8a90d6af6d037dbb5
Matt Lovett <mattlovett@shaw.ca>
2023-05-31 17:00:24 -0400

    adding check to not fire anymore process when the previous one is still running

--- DivineConsoleAppTC/Service1.vb
+++ DivineConsoleAppTC/Service1.vb
@@ -1,5 +1,11 @@
-﻿Public Class Service1
+﻿Imports System.Configuration
+Imports System.IO
+Imports Microsoft.VisualBasic.Devices
+
+Public Class Service1
     Dim tmr As Timers.Timer
+       Dim isRunning As Boolean = False ' Add a boolean variable to keep track of whether the process is running.
+
     Protected Overrides Sub OnStart(ByVal args() As String)
         ' Add code here to start your service. This method should set things
         ' in motion so your service can do its work.
@@ -15,6 +21,17 @@
     End Sub
 
     Private Sub MyTickHandler(obj As Object, e As EventArgs)
-        DivineConsoleAppTC.DvineApp.Main()
+        If Not isRunning Then ' Check if the process is already running.
+            Try
+                isRunning = True ' Set the isRunning flag to indicate that the process is now running.
+                DivineConsoleAppTC.DvineApp.Main()
+
+            Catch ex As Exception ' Catch any exceptions thrown by the AutoPost method.
+                isRunning = False ' Set the isRunning flag to indicate that the process is now running.
+            Finally
+                isRunning = False ' Set the isRunning flag to indicate that the process is no longer running.
+            End Try
+         End If
+
     End Sub
 End Class
